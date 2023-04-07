a24475dd2eb01e2d6d617fc17de2d5cd1d63e7f1
unknown <hanmari.code@gmail.com>
2023-04-07 19:31:55 +0900

    [추가] 로그인 로직 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from accounts import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='sign_up'),
+    path('signup/', views.signup, name='sign_up'), # 회원가입 페이지
+    path('login/', views.login, name='login'), # 로그인 페이지
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -25,3 +25,20 @@ def signup(request):
             new_user.bio = bio
             new_user.save()
         return redirect('/login')
+    
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html')
+    
+    # 로그인시 실행되는 로직
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = UserModel.objects.get(username=username)  # 사용자 불러오기
+        if me.password == password:  # 저장된 사용자의 패스워드와 입력받은 패스워드 비교
+            request.session['user'] = me.username  # 세션에 사용자 이름 저장
+            return redirect('/erp')
+        else: # 로그인이 실패하면 다시 로그인 페이지를 보여주기
+            return redirect('/login')
+    # 로그인 view
